1dd0b5723ff6a88c3045b8873e10353af3481aa7
Will Speak <lithiumflame@gmail.com>
2016-12-05 21:40:32 +0000

    Day 5 - Challenge 2
    More brute forcing. Not very elegant.

--- examples/day5.rs
+++ examples/day5.rs
@@ -2,8 +2,13 @@ extern crate advent;
 
 use advent::day5::*;
 
+use std::thread;
+
 const INPUT: &'static str = "ojvtpuvg";
 
 pub fn main() {
-    println!("{}: {}", INPUT, get_password(INPUT));
+    let basic = thread::spawn(|| get_password(INPUT));
+    let indexed = thread::spawn(|| get_indexed_password(INPUT));
+    println!("{} (basic): {}", INPUT, basic.join().unwrap());
+    println!("{} (indexed): {}", INPUT, indexed.join().unwrap());
 }
--- src/day5.rs
+++ src/day5.rs
@@ -2,8 +2,6 @@
 
 extern crate crypto;
 
-use std;
-
 use self::crypto::md5::Md5;
 use self::crypto::digest::Digest;
 
@@ -16,15 +14,43 @@ pub fn hash_for_suffix(door: &str, hasher: &mut Md5, suffix: i64) -> String {
 
 pub fn get_password(door: &str) -> String {
     let mut hasher = Md5::new();
-    (0..std::i64::MAX).filter_map(|i| {
-        let hash = hash_for_suffix(door, &mut hasher, i);
-        if hash.starts_with("00000") {
-            println!("found hash {} at {}", hash, i);
-            hash.chars().nth(5)
-        } else {
-            None
+    (0..)
+        .filter_map(|i| {
+            let hash = hash_for_suffix(door, &mut hasher, i);
+            if hash.starts_with("00000") {
+                Some(hash)
+            } else {
+                None
+            }
+        })
+        .filter_map(|hash| hash.chars().nth(5))
+        .take(8)
+        .collect()
+}
+
+pub fn get_indexed_password(door: &str) -> String {
+    let mut result = vec!['_','_','_','_','_','_','_','_'];
+    let mut hasher = Md5::new();
+    let mut remaining = 8;
+    for hash in (0..)
+        .filter_map(|i| {
+            let hash = hash_for_suffix(door, &mut hasher, i);
+            if hash.starts_with("00000") {
+                Some(hash)
+            } else {
+                None
+            }
+        }) {
+            let i = hash.chars().nth(5).unwrap() as usize - '0' as usize;
+            if i < 8 && result[i] == '_' {
+                result[i] = hash.chars().nth(6).unwrap();
+                remaining -= 1;
+                if remaining == 0 {
+                    break;
+                }
+            }
         }
-    }).take(8).collect::<String>()
+    result.into_iter().collect()
 }
 
 #[cfg(test)]
@@ -46,8 +72,13 @@ mod test {
         assert!(hash_for_suffix("abc", &mut hasher, 5017308).starts_with("000008f82"));
     }
 
+    // #[test]
+    // fn example_password_decrypt() {
+    //     assert_eq!("18f47a30", get_password("abc"));
+    // }
+
     #[test]
-    fn example_password_decrypt() {
-        assert_eq!("18f47a30", get_password("abc"));
+    fn example_indexed_password() {
+        assert_eq!("05ace8e3", get_indexed_password("abc"));
     }
 }
